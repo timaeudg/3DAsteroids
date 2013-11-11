68c79cd7a8cf6f04ed1b67633a5767d784273f04
timaeudg <timaeudg@rose-hulman.edu>
2013-11-11 00:59:08 -0500

    Added basic stuff for Asteroids and generation, still need to add stuff for movement, and refine creation

---
+++ src/Asteroid.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "glew/glew.h"
+#include "Triangle.h"
+#include <vector>
+#include <map>
+#include <stdint.h>
+
+using namespace std;
+class Asteroid
+{
+public:
+	Asteroid(void);
+	~Asteroid(void);
+	vector<GLfloat> getNormals();
+	vector<GLfloat> getVertices();
+	vector<GLuint> getElementArray();
+private:
+	static vector<Point> icoPoints;
+	static vector<Triangle> icoTriangles;
+	static map<int64_t, int> middlePointCache;
+	static bool randSeeded;
+	vector<Point> asteroidPoints;
+	vector<Triangle> asteroidTriangles;
+	vector<GLfloat> asteroidGLVertices;
+	vector<GLfloat> asteroidGLNormals;
+	vector<GLuint> elements;
+	void generateAsteroidBase();
+	void createIcosahedronPoints();
+	void createIcosahedronTriangles();
+	void deformAndTransformBase();
+	void deformAsteroid(vector<Point>* points);
+	void copyBaseInformation();
+	void transformToGL();
+	int calculateMiddlePoint(int p1, int p2, vector<Point>* points);
+	void refineIcoSphere();
+	int affixVertex(Point point, Point derpPoint, vector<Point>* pointsVector);
+};
+
+
+
---
+++ src/Point.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "glew/glew.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include <vector>
+#include <math.h>
+#include <ctime>
+using namespace std;
+class Point
+{
+public:
+	glm::vec3 coords;
+	Point(GLfloat x, GLfloat y, GLfloat z){
+		this->coords = glm::vec3(x, y, z);
+	}
+
+	Point();
+	~Point(void);
+
+	bool moveInRandomDirection(GLfloat maxDistanceToMove, bool seeded){
+		GLfloat maxDimensionDistance = pow(maxDistanceToMove, 2)/3.0;
+		if(!seeded){
+			time_t timevar;
+			time(&timevar);
+			srand(timevar);
+		}
+		GLfloat xToMove = -maxDimensionDistance + (float)rand()/((float)RAND_MAX/(maxDimensionDistance-(-maxDimensionDistance)));
+		GLfloat yToMove = -maxDimensionDistance + (float)rand()/((float)RAND_MAX/(maxDimensionDistance-(-maxDimensionDistance)));
+		GLfloat zToMove = -maxDimensionDistance + (float)rand()/((float)RAND_MAX/(maxDimensionDistance-(-maxDimensionDistance)));
+
+		this->coords.r += xToMove;
+		this->coords.g += yToMove;
+		this->coords.b += zToMove;
+		return true;
+	}
+};
+
+
+
