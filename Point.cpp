df1c8c17b85eb8f80a13c0020e9f176b918907a0
Devon Timaeus <timaeudg@rose-hulman.edu>
2013-11-13 16:23:29 -0500

    Adding all of this for the demo so in case things die it's okay

--- Point.cpp
+++ Point.cpp
@@ -1,9 +1,5 @@
 #include "Point.h"
 
-Point::Point(GLfloat x, GLfloat y, GLfloat z)
-{
-	this->coords = glm::vec3(x,y,z);
-}
 
 
 Point::~Point(void)
--- Triangle.cpp
+++ Triangle.cpp
@@ -1,9 +1,5 @@
 #include "Triangle.h"
 
-Triangle::Triangle(const Point p1, const Point p2, const Point p3)
-{
-	
-}
 
 
 Triangle::~Triangle(void)
